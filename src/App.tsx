a02d015504d3cfab9ff6f01074e9d560bf1694d7
João Vítor <joaovsoutosantos@gmail.com>
2022-06-23 22:22:02 -0300

    feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Event } from './pages/Event';
+
 export function App() {
-  return <h1>alskdj</h1>;
+  return <Event />;
 }
---
+++ src/components/Lesson.tsx
@@ -0,0 +1,3 @@
+export function Lesson() {
+  return <h1>lesson</h1>;
+}
