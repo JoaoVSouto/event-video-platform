fb20e11a95be7b5c662a9440b2dfc37d356bbfcc
João Vítor <joaovsoutosantos@gmail.com>
2022-06-23 21:38:14 -0300

    chore: add .env.example

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,3 @@
-import { gql, useQuery } from '@apollo/client';
-
-const GET_LESSONS_QUERY = gql`
-  query GetLessons {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-type Lesson = {
-  id: string;
-  title: string;
-};
-
 export function App() {
-  const { data, loading } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-
-  return loading ? (
-    <p>Loading...</p>
-  ) : (
-    <ul>
-      {data?.lessons.map(lesson => (
-        <li key={lesson.id}>{lesson.title}</li>
-      ))}
-    </ul>
-  );
+  return <h1>alskdj</h1>;
 }
